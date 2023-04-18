8bac915740feb605b067eb91645066d1793a84a3
elitesuper <tonylobbin7712@gmail.com>
2023-04-18 16:03:25 -0700

    added

---
+++ src/constants/Status.js
@@ -0,0 +1,6 @@
+export const StatusData = {mydata : [
+    { name: ["Automatic","Automatisch", "автоматический", "автоматический"]},
+    { name: ["Employs","Beschäftigt","Нанимает","Нанимает"]},
+    { name: ["Inaccessible", "Nicht zugänglich", "недоступный", "недоступный"]},
+    { name: ["Ready to help", "Bereit zu helfen", "Готовы помочь", "Готовы помочь"]}
+]}
